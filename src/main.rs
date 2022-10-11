7a8c7f7ba6c3c48ab24e77b3e167b91cf1bc8b03
Wink Saville <wink@saville.com>
2022-10-10 17:39:29 -0700

    refactor: Try to use std::any::Any
    See the comment at the top of src/main.rs and the Discord comment here:
      https://discord.com/channels/273534239310479360/1028428961937641592/1029167534097834044

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+// This is an attempt to implement bruh![moment] suggestion to use std::any::Any
+// but I'm unable to get it to compile. I'm probably approaching it totally wrong.
+// See: https://discord.com/channels/273534239310479360/1028428961937641592/1029167534097834044
+//
+// Also adding as_any and equals would seem to extra dynamic
+// dispatching and if my use of std::ptr::eq is fragile then
+// it seems better to go back to using SwitchSm::light_on. I
+// suspect this would be more preformant then the extra dynamic
+// dispatching.
+
 // Trait for processing actions in a State
 pub trait State<SM, P> {
+    fn as_any(&self) -> &dyn std::any::Any;
+    fn equals(&self, _other: &dyn State<SM, P>) -> bool;
     fn process(&self, sm: &mut SM, msg: &P);
 }
 
@@ -29,9 +41,40 @@ struct SwitchSm<'a> {
 }
 
 // State off
+#[derive(PartialEq)]
 struct StateOff;
 
 impl<'a> State<SwitchSm<'a>, Protocol1> for StateOff {
+    //-----
+    // Having to duplicate this code here and in StateOn isn't good
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    fn equals(&self, other: &dyn State<SwitchSm<'a>, Protocol1>) -> bool {
+        let o_any = other.as_any();
+        let o_downcast_ref = o_any.downcast_ref::<&dyn State<SwitchSm<'a>, Protocol1>>();
+                                                    // compile error ^^^^^^^^^^^^ requires that `'a` must outlive `'static`
+
+        let s_any = self.as_any();
+        let s_downcast_ref = s_any.downcast_ref::<&dyn State<SwitchSm<'a>, Protocol1>>();
+
+        let r = if let Some(o) = o_downcast_ref {
+            if let Some(s) = s_downcast_ref {
+                s == o
+            } else {
+                false
+            }
+        } else {
+            false
+        };
+
+        //let r = o_downcast_ref.map_or(false, |o| self == *o);
+
+        r
+    }
+    //-----
+
     fn process(&self, sm: &mut SwitchSm<'a>, msg: &Protocol1) {
         //println!("StateOff:+ sm.current_state={:p}", sm.current_state);
         match msg {
@@ -49,6 +92,36 @@ impl<'a> State<SwitchSm<'a>, Protocol1> for StateOff {
 struct StateOn;
 
 impl<'a> State<SwitchSm<'a>, Protocol1> for StateOn {
+    //-----
+    // Having to duplicate this code here and in StateOff isn't good
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    fn equals(&self, other: &dyn State<SwitchSm<'a>, Protocol1>) -> bool {
+        let o_any = other.as_any();
+        let o_downcast_ref = o_any.downcast_ref::<&dyn State<SwitchSm<'a>, Protocol1>>();
+                                                    // compile error ^^^^^^^^^^^^ requires that `'a` must outlive `'static`
+
+        let s_any = self.as_any();
+        let s_downcast_ref = s_any.downcast_ref::<&dyn State<SwitchSm<'a>, Protocol1>>();
+
+        let r = if let Some(o) = o_downcast_ref {
+            if let Some(s) = s_downcast_ref {
+                s == o
+            } else {
+                false
+            }
+        } else {
+            false
+        };
+
+        //let r = o_downcast_ref.map_or(false, |o| self == *o);
+
+        r
+    }
+    //-----
+
     fn process(&self, sm: &mut SwitchSm<'a>, msg: &Protocol1) {
         //println!("StateOn:+ sm.current_state={:p}", sm.current_state);
         match msg {
